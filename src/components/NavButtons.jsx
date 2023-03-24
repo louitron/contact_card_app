3af2e38cd7289480b057eb1ba0772b1d5c304fd2
louitron <luisgarcia1437@gmail.com>
2023-03-24 18:48:08 +0000

    added icons for nav buttons

--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -1,3 +1,7 @@
+import { AiFillContacts } from "react-icons/ai";
+import { RiUserAddFill } from "react-icons/ri";
+import { AiFillHome } from "react-icons/ai";
+
 export const NavButtons = () => {
   return (
     <div class="relative z-10 flex justify-center overflow-auto items-center gap-5">
@@ -5,57 +9,23 @@ export const NavButtons = () => {
         ontouchstart=""
         class="group flex items-center justify-center rounded-lg bg-slate-200 px-4 py-3 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]"
       >
+        <span class="sr-only">Explore</span>
+        <AiFillHome className="scale-150 text-gray-500" />
+      </button>
+      <button
+        ontouchstart=""
+        class="group flex items-center justify-center rounded-lg bg-slate-200 px-4 py-3 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]"
+      >
         <span class="sr-only">Home</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="h-6 w-6 text-slate-500 group-active:scale-95"
-        >
-          <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
-          <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
-        </svg>
+        <AiFillContacts className="scale-150 text-gray-500" />
       </button>
       <button
         ontouchstart=""
         class="group flex items-center justify-center rounded-lg bg-slate-200 px-4 py-3 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]"
       >
         <span class="sr-only">Account</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="h-6 w-6 text-slate-500 group-active:scale-95"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
+        <RiUserAddFill className="text-gray-500 scale-125" />
       </button>
-
-      <button
-        ontouchstart=""
-        class="group flex items-center justify-center rounded-lg bg-slate-200 px-4 py-3 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]"
-      >
-        <span class="sr-only">Explore</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="h-6 w-6 text-slate-500 group-active:scale-95"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </button>
-
-      
-     
     </div>
   );
 };
